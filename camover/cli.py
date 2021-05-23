15a34e3bd92624f67283677e5235a0ca0e8f6597
Ivan Nikolsky <enty8080@gmail.com>
2021-05-23 16:32:09 +0300

    Update cli.py

--- camover/cli.py
+++ camover/cli.py
@@ -94,7 +94,7 @@ class CamOverCLI(CamOver, Badges):
                     with open(self.args.output, 'a') as f:
                         f.write(f"{result}\n")
         else:
-            self.print_error("No list or address specified!")
+            parser.print_help()
 
 def main():
     cli = CamOverCLI()
